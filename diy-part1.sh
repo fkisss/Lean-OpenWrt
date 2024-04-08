17c4b8cdef326a2edc757c454e003c21579d9d2a
fkisss <61588117+fkisss@users.noreply.github.com>
2024-04-08 21:46:40 +0800

    Update diy-part1.sh
    修改软件源

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@
 
 # 以下自己增加
 
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-
-
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo 'src-git openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/aarch64_cortex-a53/kiddin9' >>feeds.conf.default
