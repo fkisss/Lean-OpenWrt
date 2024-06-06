0aaf4973bf9f48623042ccda9d9c6b7cc98d90b9
fkisss <61588117+fkisss@users.noreply.github.com>
2024-06-06 23:12:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,4 +24,5 @@
 
 sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '2i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
