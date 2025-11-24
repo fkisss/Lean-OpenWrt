84971c90cb274cba2098c998e0a2cd7b14d91d71
fkisss <61588117+fkisss@users.noreply.github.com>
2025-11-24 23:16:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
+echo 'src-git tailscale https://github.com/tailscale/tailscale-openwrt.git' >> feeds.conf.default
 # 以下自己增加
 
 # echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
