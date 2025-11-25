9b71b56b5117e1eec4da329c3fc1c5660e55a2bc
fkisss <61588117+fkisss@users.noreply.github.com>
2025-11-25 13:17:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,8 +20,9 @@
 
 # echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# 尝试添加不同的 Tailscale OpenWrt 包源
+# sed -i '1i src-git tailscale https://github.com/adyanth/openwrt-tailscale-enabler.git' feeds.conf.default
 
-sed -i '1i src-git tailscale https://github.com/tailscale/tailscale.git' feeds.conf.default
 sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
