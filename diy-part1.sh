91696936f6bb56d9da88fc56e8c987e4ff11d642
fkisss <61588117+fkisss@users.noreply.github.com>
2026-03-03 11:41:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,8 +23,8 @@
 # 尝试添加不同的 Tailscale OpenWrt 包源
 # sed -i '1i src-git tailscale https://github.com/adyanth/openwrt-tailscale-enabler.git' feeds.conf.default
 
-sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+sed -i '1i src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall' feeds.conf.default
+sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages' feeds.conf.default
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 ./scripts/feeds update -a
