8a9562cc91b1d1496de591138905215da300428c
fkisss <61588117+fkisss@users.noreply.github.com>
2026-03-04 10:42:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,7 +25,7 @@
 
 sed -i '1i src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall' feeds.conf.default
 sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages' feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
 ./scripts/feeds update -a
 rm -rf feeds/passwall_packages/shadowsocksr-libev
